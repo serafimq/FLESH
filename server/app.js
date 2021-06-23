d4abf35e00e993c948866bdb2036aa4018b4ed9c
Yaroslav <serafim90@mail.ru>
2021-06-23 10:10:48 +0300

    chenge app server

--- server/app.js
+++ server/app.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
 const path = require('path')
 const cors = require('cors')
 
 
-const indexRouter = require('./src/routes/indexRouter');
-const userRouter = require('./src/routes/userRouter');
-const dbConnect = require('./src/config/dbConnect');
+const indexRouter = require('./src/routes/indexRouter')
+const userRouter = require('./src/routes/userRouter')
+const dbConnect = require('./src/config/dbConnect')
 
-const app = express();
+const app = express()
 const { PORT } = process.env
 
 app.use(express.static(path.resolve('../client/build')))
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 // подключаем роутеры
-app.use('/', indexRouter)
+// app.use('/', indexRouter)
 app.use('/user', userRouter)
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve('../client/build/index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('../client/build/index.html'))
+})
 
 
 // стартуем наш сервер
